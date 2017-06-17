cd958f8883802e36c95e2b46d11db9eaf4ee0782
parksanguk <psu9808@naver.com>
2017-06-17 20:47:47 +0900

    Send Data(Dur,temp_obj) to Server

--- Sensor/sensor/sensor.ino
+++ Sensor/sensor/sensor.ino
@@ -69,39 +69,17 @@ String sendData(String command, const int timeout, boolean debug){
   return response;
 }
 
-void send_post_packet(float temp_ambient,float temp_object,float rel_hum,float temp,float hic){
+void send_post_packet(float duration,float temp_object){
   Serial.println("Starting connection to server...");
-  String content="?amb="+String(temp_ambient)+"&obj="+String(temp_object)+"&hum="+String(rel_hum);
+  String content="?dur="+String(duration)+"&obj="+String(temp_object);
 
   sendData("AT+CIPSTART=\"TCP\",\""+private_server+"\", "+String(serverPort)+"\r\n",1000,DEBUG);
-  //Serial.println("AT+CIPSTART=\"TCP\",\""+private_server+"\", "+String(serverPort));
-
-  /*if(esp8266.find("OK")){
-    Serial.println("TCP connection ready");
-  } delay(1000);*/
-  //sendData("AT+CIPMODE=1\r\n",1000,DEBUG);
-  //delay(1000);
 
   String postReq="GET /getdata"+content+" HTTP/1.0\r\n\r\n";
-  //              +"\r\nAccept: */**\r\nContent-Length: "+String(content.length())
-  //              +"\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n";
   String sendCmd="AT+CIPSEND="+String(postReq.length())+"\r\n";
   sendData(sendCmd,500,DEBUG);
   sendData(postReq,1000,DEBUG);
-  //Serial.println(esp8266.readString());
-  /*if(esp8266.find(">")) { 
-    Serial.println("Sending..");
-    esp8266.print(postReq);
-    if(esp8266.find("SEND OK")){
-      Serial.println("Packent sent");
-      while(esp8266.available()){
-        String tmpResp=esp8266.readString();
-        delay(1000);
-        Serial.println(tmpResp);
-      }
-      esp8266.println("AT+CIPCLOSE");
-    }
-  }*/
+  
   sendData("AT+CIPCLOSE\r\n",1000,DEBUG);
 }
  
@@ -110,7 +88,8 @@ void setup() {
   mp3Serial.begin(9600);
   mp3_set_serial (mp3Serial);      // DFPlayer-mini mp3 module 시리얼 세팅
   delay(1);                     // 볼륨값 적용을 위한 delay
-  mp3_set_volume (15);          // 볼륨조절 값 0~30
+  mp3_set_volume (20);          // 볼륨조절 값 0~30
+  mp3_stop();
   
   esp8266.begin(9600);
   //sendData("AT+RST",2000,DEBUG);
@@ -138,15 +117,19 @@ void setup() {
   
 }
 
-int dist_measure(){
+long dist_measure(){
 
-  int duration, distance; //Adding duration and distance
-  digitalWrite(triggerPin, HIGH); //triggering the wave(like blinking an LED)
-  delay(10);
-  digitalWrite(triggerPin, LOW);
-  duration = pulseIn(echoPin, HIGH); //a special function for listening and waiting for the wave
-  distance = (duration/2) / 29.1; //transforming the number to cm(if you want inches, you have to change the 29.1 with a suitable number
-  delay(10);
+  long duration, distance=0; //Adding duration and distance
+  while(distance==0){
+    digitalWrite(triggerPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(triggerPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(triggerPin, LOW);
+    duration = pulseIn(echoPin, HIGH,32760); //a special function for listening and waiting for the wave
+    distance = (duration/2) / 29.1; //transforming the number to cm(if you want inches, you have to change the 29.1 with a suitable number
+    delay(10);
+  }
   Serial.print(distance); //printing the numbers
   Serial.println("cm"); //and the unit
   
@@ -155,26 +138,53 @@ int dist_measure(){
 
 #define interval 1000
 unsigned long mark=0;
+boolean playing_flag=0;
+float temp_object_max=0;
+int max_dist=50;
 void loop() {
   boolean play_state = digitalRead(8);
   float temp_ambient,temp_object;
   float rel_hum,temp,hic;
-  int distance;
+  long distance;
   
-  if(millis()>mark){
-    mark=millis()+interval;
-    distance=dist_measure();
+  //mark=millis()+interval;
+  distance=dist_measure();
+ // if(distance<200){
+    mlx_measure(&temp_ambient,&temp_object);
+    if(temp_object>temp_object_max) temp_object_max=temp_object;
     
-    while(distance<100){
-      mlx_measure(&temp_ambient,&temp_object);
+    if(playing_flag==0 &&distance<max_dist){
+      //MP3 Start
+      mark=millis();
+      playing_flag=1;
       mp3_play(1);
-      delay(5000);
-      distance=dist_measure();
-    }
-    mp3_stop();
-//Serial.println(" "); //just printing to a new line
+      delay(4000);      
+    }  
+    else if(playing_flag==1 && distance>=max_dist){
+      //Drop in middle
+      int sum=0;
+      for(int i=0;i<10;i++){
+        if(dist_measure()>=max_dist) sum++;
+      }
+      if(sum>=9){
+        temp_object_max=0;
+        mp3_stop();
+        playing_flag=0;
+        float duration=(millis()-mark)/1000;
+        send_post_packet(duration,temp_object);
 
-    //send_post_packet(temp_ambient,temp_object,rel_hum,temp,hic);
-  //delay(10000);
-  }
+      }
+    }
+    else if(playing_flag==1 && play_state==HIGH){
+      //LISTEN UNTIL END
+      temp_object_max=0;
+      playing_flag=0;
+      float duration=(millis()-mark)/1000;
+      send_post_packet(duration,temp_object);
+     
+    }
+ // }
+
+  delay(1000);
+  
 }
